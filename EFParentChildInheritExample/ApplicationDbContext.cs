1c1f089bcb351ace31337a62149825b2cad92d80
Wade <35878500+wadebaird@users.noreply.github.com>
2023-05-22 10:42:33 -0700

    Add project files.

---
+++ EFParentChildInheritExample/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFParentChildInheritExample;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<BaseClass> BaseClasses { get; set; }
+    public DbSet<DerivedClassOne> DerivedClassOnes { get; set; }
+    public DbSet<DerivedClassTwo> DerivedClassTwos { get; set; }
+    public DbSet<DerivedClassThree> DerivedClassThrees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(x => x.MigrationsHistoryTable("MigrationHistory"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<DerivedClassOne>()
+            .HasMany(e => e.Children)
+            .WithOne(e => (DerivedClassOne)e.ParentClass)
+            .HasForeignKey(e => e.ParentId)
+            .HasPrincipalKey(e => e.Id);
+
+        modelBuilder.Entity<DerivedClassTwo>()
+            .HasMany(e => e.Children)
+            .WithOne(e => (DerivedClassTwo)e.ParentClass)
+            .HasForeignKey(e => e.ParentId)
+            .HasPrincipalKey(e => e.Id);
+
+        modelBuilder.Entity<DerivedClassThree>()
+            .HasOne(e => e.Child)
+            .WithOne(e => (DerivedClassThree)e.ParentClass)
+            .HasForeignKey<ChildClass>(e => e.ParentId)
+            .HasPrincipalKey<DerivedClassThree>(e => e.Id);
+    }
+}
---
+++ EFParentChildInheritExample/BaseClass.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFParentChildInheritExample;
+
+[Table("Base")]
+public abstract class BaseClass
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string PropertyOne { get; set; }
+}
+
+public class DerivedClassOne : BaseClass
+{
+    public ICollection<ChildClass> Children { get; set; }
+}
+
+public class DerivedClassTwo : BaseClass
+{
+    public ICollection<ChildClass> Children { get; set; }
+}
+
+public class DerivedClassThree : BaseClass
+{
+    public ChildClass Child { get; set; }
+}
+
+[Table("Children")]
+[PrimaryKey(nameof(Id), nameof(ParentId))]
+public class ChildClass
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public int ParentId { get; set; }
+    public BaseClass ParentClass { get; set; }
+
+    public string PropertyX { get; set; }
+}
